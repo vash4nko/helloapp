f4ddaef07572cb24fb0a624584294f2588bdf7e5
black <black@example.com>
2026-07-10 18:58:39 +0500

    Call GreetWhite and GreetBlack methods

--- Program.cs
+++ Program.cs
@@ -4,3 +4,17 @@ void GreetBlack()
 {
     Console.WriteLine("hello black");
 }
+
+void GreetWhite()
+{
+    Console.WriteLine("hello white");
+}
+
+void GreetBlack()
+{
+    Console.WriteLine("hello black");
+}
+
+Console.WriteLine("hello everyone");
+GreetWhite();
+GreetBlack();
